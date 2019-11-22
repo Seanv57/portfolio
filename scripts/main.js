5d8e4fdf276db022c8bc64233049be9131afeaf8
Sean <seanv57@gmail.com>
2019-11-22 13:45:14 -0700

    Fix CSS and Responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,24 @@
-function myFunction() {
-  var x = document.getElementById("topnav");
-  if (x.className === "nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav";
-  }
+/* const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('.nav-active');
+  });
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = ''
+    } else {
+    link.style.animation = 'navLinkFade 0.5s ease forwards $(index / 7 + 1.5)s';
+    }
+  });
 }
+
+navSlide();
+*/
+/* $(document).ready(function(){
+  $('.fa-bars').on('click', function() {
+      $('.dropdown').css('display','block');
+  })
+}
+*/
