62360afce99520521c9f1cb6af23978581ef799c
Sean <seanv57@gmail.com>
2019-11-25 16:31:18 -0700

    Refactor code

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,8 @@
-/* const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('.nav-active');
-  });
-  navLinks.forEach((link, index) => {
-    if (link.style.animation) {
-      link.style.animation = ''
-    } else {
-    link.style.animation = 'navLinkFade 0.5s ease forwards $(index / 7 + 1.5)s';
-    }
-  });
+function toggleClass() {
+  let menu = document.querySelector(".menu");
+  menu.classList.toggle("toggleCls");
 }
 
-navSlide();
-*/
-/* $(document).ready(function(){
-  $('.fa-bars').on('click', function() {
-      $('.dropdown').css('display','block');
-  })
-}
-*/
+let burger = document.querySelector(".icon");
+
+burger.addEventListener("click", toggleClass);
