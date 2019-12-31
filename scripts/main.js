a7b6c7f41b610eb2d4f62ee13e42894438270e95
Sean <seanv57@gmail.com>
2019-12-31 11:38:39 -0700

    Updates from the week of Dec 23rd

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// Menu navigation
 function toggleClass() {
   let menu = document.querySelector(".menu");
   menu.classList.toggle("toggleCls");
@@ -6,3 +7,22 @@ function toggleClass() {
 let burger = document.querySelector(".icon");
 
 burger.addEventListener("click", toggleClass);
+
+topbutton = document.getElementById("backToTop");
+
+//Back to top button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
