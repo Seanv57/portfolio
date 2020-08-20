7a5d135fbf8f84675b09dd99a0e4d1222262ccba
Sean <seanv57@gmail.com>
2020-08-19 18:32:57 -0700

    Complete redesign

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,34 @@
 // Menu navigation
-function toggleClass() {
-  let menu = document.querySelector(".menu");
-  menu.classList.toggle("toggleCls");
-}
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelector(".menu-items");
+const links = document.querySelectorAll(".menu-items li");
+const work = document.querySelector(".work");
 
-let burger = document.querySelector(".icon");
-
-burger.addEventListener("click", toggleClass);
-
-var topButton = document.getElementById("backToTop");
+menu.addEventListener("click", () => {
+  menu.classList.toggle("menux");
+  menuItems.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
 
 //Back to top button
+const topButton = document.getElementById("backToTop");
+
 if (topButton != null) {
-  window.onscroll = function() {scrollFunction()};
+ window.onscroll = function() {scrollFunction()};
 }
 
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      topButton.style.display = "block";
-    } else {
-      topButton.style.display = "none";
-    }
-  }
-
-
+function scrollFunction() {
+ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+   topButton.style.display = "block";
+ } else {
+   topButton.style.display = "none";
+ }
+}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+ document.body.scrollTop = 0;
+ document.documentElement.scrollTop = 0;
 }
